4e003129569af78ad2f62262fe12ee6787ab6c16
Toey <compro222@gmail.com>
2020-07-21 16:00:47 +0700

    "Template stock create/edit"

--- frontend/src/app/stock/stock-create/stock-create.component.ts
+++ frontend/src/app/stock/stock-create/stock-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-create',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-create.component.css']
 })
 export class StockCreateComponent implements OnInit {
+  imageSrc: string | ArrayBuffer;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmitForm(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    alert(form.value.name);
+  }
+
+  onPreviewImageChange(event){
+    const metaImage = event.target.files[0]
+    if (metaImage) {
+      //this.file = metaImage
+
+      const reader = new FileReader()
+      reader.readAsDataURL(metaImage)
+      reader.onload = () => {
+        this.imageSrc = reader.result
+      }
+    }
   }
 
 }
--- frontend/src/app/stock/stock-edit/stock-edit.component.ts
+++ frontend/src/app/stock/stock-edit/stock-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-edit',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-edit.component.css']
 })
 export class StockEditComponent implements OnInit {
+  imageSrc: string | ArrayBuffer;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmitForm(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    alert(form.value.name);
+  }
+
+  onPreviewImageChange(event){
+    const metaImage = event.target.files[0]
+    if (metaImage) {
+      //this.file = metaImage
+
+      const reader = new FileReader()
+      reader.readAsDataURL(metaImage)
+      reader.onload = () => {
+        this.imageSrc = reader.result
+      }
+    }
   }
 
 }
