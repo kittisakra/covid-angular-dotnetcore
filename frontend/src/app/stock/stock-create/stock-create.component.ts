b8d48d4827a9dcdf54dba5b64f2328fe0707aaa9
Toey <compro222@gmail.com>
2020-07-24 11:49:27 +0700

    button new product/ stock create

--- frontend/src/app/stock/stock-create/stock-create.component.ts
+++ frontend/src/app/stock/stock-create/stock-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { NetworkService } from 'src/app/services/network.service';
+import { Product } from 'src/app/models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stock-create',
@@ -8,8 +11,12 @@ import { NgForm } from '@angular/forms';
 })
 export class StockCreateComponent implements OnInit {
   imageSrc: string | ArrayBuffer;
+  file: File;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private networkService: NetworkService,
+  ) { }
 
   ngOnInit(): void {
   }
@@ -18,13 +25,28 @@ export class StockCreateComponent implements OnInit {
     if (form.invalid) {
       return;
     }
-    alert(form.value.name);
+    const value = form.value;
+    let product = new Product();
+
+    product.name = value.name;
+    product.image = this.file;
+    product.price = value.price;
+    product.stock = value.stock;
+
+    this.networkService.addProduct(product).subscribe(
+      res => {
+        this.router.navigate(["/stock"]);
+      },
+      error => {
+        alert(error);
+      }
+    );
   }
 
-  onPreviewImageChange(event){
+  onPreviewImageChange(event) {
     const metaImage = event.target.files[0]
     if (metaImage) {
-      //this.file = metaImage
+      this.file = metaImage
 
       const reader = new FileReader()
       reader.readAsDataURL(metaImage)
